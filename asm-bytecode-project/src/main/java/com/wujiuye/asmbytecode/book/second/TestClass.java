8f83c827a6919960d6c69c866beaa9e6d24dfc9d
吴就业 <jiuye.wu@molimediagroup.com>
2021-08-15 02:13:33 +0800

    add .gitignore

---
+++ asm-bytecode-project/src/main/java/com/wujiuye/asmbytecode/book/second/TestClass.java
@@ -0,0 +1,10 @@
+package com.wujiuye.asmbytecode.book.second;
+
+public class TestClass {
+
+    private static float TEST_FLOAT = 100F;
+    private static long TEST_LONG = 100L;
+    private static double TEST_DOUBLE = 100d;
+    private static int TEST_INT = 100;
+
+}
